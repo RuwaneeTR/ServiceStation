148cd9f63bae18604a71ef3fb12741b24f8d6a8d
Ruwanee Rathnayaka <ruwanithakshila2003@gmail.com>
2026-01-02 01:45:53 +0530

    Add project files.

---
+++ ServiceStation.Web/Controllers/ServicesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceStation.Web.Models;
+
+namespace ServiceStation.Web.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details() 
+        {
+            var service = new VehicleService() {
+                Id = 1,
+                Title = "Test",
+                Description = "change the oil",
+                cost = 39.99m,
+            };
+           
+             
+            return View(service);
+        }
+    }
+}
---
+++ ServiceStation.Web/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceStation.Web.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Full Name")]
+        [StringLength(100)]
+        [Required]
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        [Required]
+        public string Email  { get; set; }
+    }
+}
